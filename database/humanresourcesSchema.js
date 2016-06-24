6586b63aa83173c67dcbb152e8bf6748ed56a07e
bluemagma612 <bryce@bluemagma.com>
2016-06-24 13:43:17 -0500

    connecting to db and created 3 teams

--- database/humanresourcesSchema.js
+++ database/humanresourcesSchema.js
@@ -1,13 +1,45 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var db = mongoose.connection;
-var dbUrl = 'mongodb://localhost/humanresources';
+var dbUrl = 'mongodb://localhost/humanResources';
 
 var TeamSchema = new Schema({
 	name: {
 		type: String,
 		required: true
 	}
+});
+
+var Team = mongoose.model('Team', TeamSchema);
+
+db.on('error', function () {
+	console.log('there was an error communicating with the database');
+});
+
+mongoose.connect(dbUrl, function (err) {
+	if (err) {
+		return console.log('there was a problem connecting to this here database' + err);
+	}
+	console.log('connected!!!!');
+
+	Team.create([{
+		name: 'Product Development'
+	}, {
+		name: 'Dev Ops'
+	}, {
+		name: 'Accounting'
+	}], function (error, pd, devops, acct) {
+		if (error) {
+			console.log(error);
+		} else {
+			console.dir(pd);
+			console.dir(devops);
+			console.dir(acct);
+		
+			db.close();
+			process.exit();
+		}
+	});
 });
 
 var EmployeeSchema = new Schema({
