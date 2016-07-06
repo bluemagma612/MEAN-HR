83a768b0181d502c964d46e5425bd5b13b100413
bluemagma612 <bryce@bluemagma.com>
2016-07-06 14:05:08 -0500

    updated index.js to be a real app.js for express

--- index.js
+++ index.js
@@ -1,50 +1,53 @@
-var http = require('http');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
 require('./lib/connection');
-var employeeService = require('./lib/employees');
-var responder = require('./lib/responseGenerator');
-var staticFile = responder.staticFile('/public');
+var employees = require('./routes/employees');
+var teams = require('./routes/teams');
 
+var app = express();
 
-http.createServer(function (req, res) {
-  // A parsed url to work with in case there are parameters
-  var _url;
+//app.use(favicon(__dirname + '/public/favicon/ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-  // In case the client uses lower case for methods.
-  req.method = req.method.toUpperCase();
-  console.log(req.method + ' ' + req.url);
+//app routes
+app.use(employees);
+app.use(teams);
 
-  if (req.method !== 'GET') {
-    res.writeHead(501, {
-      'Content-Type': 'text/plain'
+//catch 404 and forward to error handler
+app.use(function(req,res,next) {
+  var err = new Error('Not Found');
+
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+//dev err handler to print stack trace
+if (app.get('env') === 'development') {
+  app.use(function(err, req,res,next) {
+    res.status(err.status || 500);
+    res.send({
+      message: err.message,
+      error: err
     });
-    return res.end(req.method + ' is not implemented by this server.');
-  }
+  });
+}
 
-  if (_url = /^\/employees$/i.exec(req.url)) {
-    employeeService.getEmployees(function (error, data) {
-      if (error) {
-        return responder.send500(error, res);
-      }
-      return responder.sendJson(data, res);
-    });
-  } else if (_url = /^\/employees\/(\d+)$/i.exec(req.url)) {
-    employeeService.getEmployee(_url[1], function (error, data) {
-      if (error) {
-        return responder.send500(error, res);
-      }
+//prod err handler no stack trace
+app.use(function(err,req,res,next) {
+  res.status(err.status || 500);
+});
 
-      if (!data) {
-        return responder.send404(res);
-      }
-
-      return responder.sendJson(data,res);
-    });
-  }
-  else {
-    // try to send the static file
-    res.writeHead(200, { 'Content-Type': 'text/html'});
-    staticFile(req.url, res);    
-  }
-}).listen(1337, '127.0.0.1');
+module.exports = app;
 
 console.log('Server running at http://127.0.0.1:1337/');
