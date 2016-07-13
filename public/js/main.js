23367c261e4bbd73581953584659258029a9539d
bluemagma612 <bryce@bluemagma.com>
2016-07-13 16:00:15 -0500

    cleaning up master branch, removing unnecessary files

--- public/js/main.js
+++
@@ -1,8 +0,0 @@
-(function(a, window){var app = angular.module('app', ['ngRoute', 'ngResource']);
-app.config(['$routeProvider', function($routeProvider) {
-// See Listing 19-2 for complete code
-}]);
-app.factory('EmployeeService', ['$resource', function($resource) {
-// See Listing 19-3 for complete code
-}]);
-}(angular, window));
